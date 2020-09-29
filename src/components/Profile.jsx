2c0ece1f294b0666ae45022da8e443ae642f5423
Seb Ots <ots.sebastien@gmail.com>
2020-09-29 12:56:57 +0200

    feat v1

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
-import { Context } from "../Context"
+import { Context } from "../Context";
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -15,17 +15,16 @@ const useStyles = makeStyles(theme => ({
     marginBottom: "5rem",
   },
   avatar: {
-    marginBottom: "5rem"
-  }
+    marginBottom: "5rem",
+    marginTop: "5rem",
+  },
 }));
 
 export default function Profile(props) {
   const classes = useStyles();
-  const { recipes, cookbooks } = useContext(Context)
+  const { recipes, cookbooks } = useContext(Context);
 
-  useEffect(() => {
-
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <Grid
@@ -35,8 +34,14 @@ export default function Profile(props) {
       alignItems="center"
       className={classes.container}
     >
-      <Typography variant="h6" className={classes.title}>Marc</Typography>
-      <img className={classes.avatar} src="https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png" alt="avatar" />
+      <img
+        className={classes.avatar}
+        src="https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png"
+        alt="avatar"
+      />
+      <Typography variant="h6" className={classes.title}>
+        Sébastien
+      </Typography>
       <Typography>Nombre de recettes : {recipes.length}</Typography>
       <Typography>Nombre de livres de recettes : {cookbooks.length}</Typography>
     </Grid>
